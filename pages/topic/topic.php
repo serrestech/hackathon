18c8b220b29e7f25fcf948bdb6a1a3d47350088e
TheodorosPloumis <me@theodorosploumis.com>
2019-05-15 17:36:33 +0300

    add new link

--- pages/topic/topic.php
+++ pages/topic/topic.php
@@ -1,6 +1,6 @@
 <?php
 
-  $topic_url = "https://github.com/serrestech/hackathon/tree/gh-pages/pages/topic";
+  $topic_url = "https://github.com/serrestech/hackathon/tree/topic";
 
   header("HTTP/1.1 301 Moved Permanently");
   header("Location: " . $topic_url);
